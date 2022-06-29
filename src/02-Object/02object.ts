826b5dae1736beda7b84b1de937debbad627f1be
Vitalick <miraigeweb@gmail.com>
2022-06-29 13:03:43 +0300

    add files

---
+++ src/02-Object/02object.ts
@@ -0,0 +1,52 @@
+type LocalCityType = {
+    title: string
+    countryTitle: string
+}
+type TechType = {
+    id: number
+    title: string
+}
+type StudentType = {
+    id: number
+    name: string
+    age: number
+    status: boolean
+    address: AddressType
+    technologies: Array<TechType>
+}
+type AddressType = {
+    streetTitle: string
+    city: LocalCityType
+}
+
+const student: StudentType = {
+    id: 1,
+    name: 'Vit',
+    age: 41,
+    status: true,
+    address: {
+        streetTitle: 'Bogdanovicha',
+        city: {
+            title: 'Molodechno',
+            countryTitle: 'Belarus',
+        },
+    },
+    technologies: [
+        {
+            id: 1,
+            title: 'HTML'
+        },
+        {
+            id: 2,
+            title: 'CSS'
+        },
+        {
+            id: 3,
+            title: 'JS'
+        },
+        {
+            id: 4,
+            title: 'REACT'
+        },
+    ]
+}
